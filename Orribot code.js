d9d3f9173b7e7f22e3b597b74b9dc03472d962c2
orribu <oliveva@yahoo.com>
2017-07-09 13:03:03 -0700

    orribot test

---
+++ Orribot code.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  newUsers.set(member.id, member.user);
+
+  if (newUsers.size > 10) {
+    const userlist = newUsers.map(u => u.toString()).join(" ");
+    guild.defaultChannel.send("Hey there! Welcome aboard!\n" + userlist);
+    newUsers.clear();
+  }
+});
